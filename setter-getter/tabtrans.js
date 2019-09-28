4ec66946baa943bb5dbf9509cb1a77317421da48
wolfz <azhar.zakiy@gmail.com>
2019-09-28 21:08:09 +0700

    create setter getter tabtrans

---
+++ setter-getter/tabtrans.js
@@ -0,0 +1,124 @@
+let tabtrans = {
+    _tabtrans_id: 0,
+    _no_rekening: '',
+    _kode_kantor: '',
+    _kuitansi: '',
+    _no_rekening_vs: '',
+    _user_id: '',
+    _pokok: '',
+    _tgl_trans: '',
+    _jam: '',
+    _ip_add: '',
+    _kode_trans: '',
+    _keterangan: '',
+    _kuitansi_id: '',
+    _sandi_trans: '',
+    _verifikasi: '',
+    _kode_integrasi: '',
+    set tabtrans_id(tabtrans_id) {
+        this._tabtrans_id = tabtrans_id;
+    },
+    get tabtrans_id() {
+        return this._tabtrans_id;
+    },
+    set no_rekening(no_rekening) {
+        this._no_rekening = no_rekening;
+    },
+    get no_rekening() {
+        return this._no_rekening;
+    },
+    set kode_kantor(kode_kantor) {
+        this._kode_kantor = kode_kantor;
+    },
+    get kode_kantor() {
+        return this._kode_kantor;
+    },
+    set kuitansi(kuitansi) {
+        this._kuitansi = kuitansi;
+    },
+    get kuitansi() {
+        return this._kuitansi;
+    },
+    set no_rekening_vs(no_rekening_vs) {
+        this._no_rekening_vs = no_rekening_vs;
+    },
+    get no_rekening_vs() {
+        return this._no_rekening_vs;
+    },
+    set user_id(user_id) {
+        this._user_id = user_id;
+    },
+    get user_id() {
+        return this._user_id;
+    },
+    set pokok(pokok) {
+        this._pokok = pokok;
+    },
+    get pokok() {
+        return this._pokok;
+    },
+    set tgl_trans(tgl_trans) {
+        this._tgl_trans = tgl_trans;
+    },
+    get tgl_trans() {
+        return this._tgl_trans;
+    },
+    set jam(jam) {
+        this._jam = jam;
+    },
+    get jam() {
+        return this._jam;
+    },
+    set ip_add(ip_add) {
+        this._ip_add = ip_add;
+    },
+    get ip_add() {
+        return this._ip_add;
+    },
+    set kode_trans(kode_trans) {
+        this._kode_trans = kode_trans;
+    },
+    get kode_trans() {
+        return this._kode_trans;
+    },
+    set keterangan(keterangan) {
+        this._keterangan = keterangan;
+    },
+    get keterangan() {
+        return this._keterangan;
+    },
+    set kuitansi_id(kuitansi_id) {
+        this._kuitansi_id = kuitansi_id;
+    },
+    get kuitansi_id() {
+        return this._kuitansi_id;
+    },
+    set sandi_trans(sandi_trans) {
+        this._sandi_trans = sandi_trans;
+    },
+    get sandi_trans() {
+        return this._sandi_trans;
+    },
+    set verifikasi(verifikasi) {
+        this._verifikasi = verifikasi;
+    },
+    get verifikasi() {
+        return this._verifikasi;
+    },
+    set kode_integrasi(kode_integrasi) {
+        this._kode_integrasi = kode_integrasi;
+    },
+    get kode_integrasi() {
+        return this._kode_integrasi;
+    },
+    set kode_integrasi_vs(kode_integrasi_vs) {
+        this._kode_integrasi_vs = kode_integrasi_vs;
+    },
+    get kode_integrasi_vs() {
+        return this._kode_integrasi_vs;
+    }
+};
+
+module.exports = {
+    tabtrans
+};
