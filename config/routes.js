75bcf6d8c8446606a68cdd31246f0c4c7e508f27
wolfz <azhar.zakiy@gmail.com>
2019-09-28 21:09:43 +0700

    adding routes application

---
+++ config/routes.js
@@ -0,0 +1,20 @@
+const utility = require('../helpers/utility');
+const routes = require('express').Router();
+const tabungan_controller = require('../controllers/tabungan_controller');
+const global_controller = require('../controllers/global_controller');
+const apicode = require('../constants/apicode');
+
+// noinspection JSUnresolvedFunction
+routes.get('/', (req, res) => {
+    return res.send(utility.GiveResponse("00", process.env.APPNAME + " Version " + process.env.VERSION));
+});
+
+// noinspection JSUnresolvedFunction
+routes.post('/' + apicode.apiCodeTransTarikTabungan + '', tabungan_controller.HandlerTransTarikTabungan);
+// noinspection JSUnresolvedFunction
+routes.post('/' + apicode.apiCodeInquirySaldo + '', tabungan_controller.HandlerInquirySaldo);
+// noinspection JSUnresolvedFunction
+routes.post('/' + apicode.apiCodeMutasiTabungan + '', tabungan_controller.HandlerMutasiTabungan);
+// noinspection JSUnresolvedFunction
+routes.post('/' + apicode.apiCodeGetKuitansi + '', global_controller.HandlerGetKuitansi);
+module.exports = routes;
