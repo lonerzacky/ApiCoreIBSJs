078ee833f6971ffa889ad4a92eac7c567d2b51be
wolfz <azhar.zakiy@gmail.com>
2019-09-28 21:07:11 +0700

    Create constant application

---
+++ constants/apicode.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    apiCodeTransTarikTabungan: '01001',
+    apiCodeInquirySaldo: '01002',
+    apiCodeMutasiTabungan: '01003',
+    apiCodeGetKuitansi: '02001'
+});
