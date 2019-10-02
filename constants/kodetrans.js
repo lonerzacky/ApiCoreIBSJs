b9ddc786ec34ada4ebaace59f73ebd43fceb754e
wolfz <azhar.zakiy@gmail.com>
2019-10-02 18:15:19 +0700

    add freeze object kode trans

---
+++ constants/kodetrans.js
@@ -0,0 +1,8 @@
+// noinspection JSUnresolvedVariable
+module.exports = Object.freeze({
+    tabungan: {
+        kodeTransTarikTunai:'200',
+        kodeTransTarikCoa:'202',
+        kodeTransTransfer:'204'
+    },
+});
