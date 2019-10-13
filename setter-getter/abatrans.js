45e3f2b78d843f50fc50c67ba1e4d08c33d1557b
wolfz <azhar.zakiy@gmail.com>
2019-10-13 11:40:35 +0700

    add abatrans setter-getter

---
+++ setter-getter/abatrans.js
@@ -0,0 +1,132 @@
+let abatrans = {
+    _abatrans_id: 0,
+    _tgl_trans: '',
+    _no_rekening: '',
+    _my_kode_trans: '',
+    _kuitansi: '',
+    _tob: '',
+    _modul_id_source: '',
+    _giro_pokok: '',
+    _tab_pokok: '',
+    _dep_pokok: '',
+    _keterangan: '',
+    _verifikasi: '',
+    _userid: '',
+    _kode_trans: '',
+    _kode_kantor: '',
+    _jam: '',
+    _ip_add: '',
+    _kuitansi_id: '',
+    set abatrans_id(abatrans_id) {
+        this._abatrans_id = abatrans_id;
+    },
+    get abatrans_id() {
+        return this._abatrans_id;
+    },
+    set tgl_trans(tgl_trans) {
+        this._tgl_trans = tgl_trans;
+    },
+    get tgl_trans() {
+        return this._tgl_trans;
+    },
+    set no_rekening(no_rekening) {
+        this._no_rekening = no_rekening;
+    },
+    get no_rekening() {
+        return this._no_rekening;
+    },
+    set my_kode_trans(my_kode_trans) {
+        this._my_kode_trans = my_kode_trans;
+    },
+    get my_kode_trans() {
+        return this._my_kode_trans;
+    },
+    set kuitansi(kuitansi) {
+        this._kuitansi = kuitansi;
+    },
+    get kuitansi() {
+        return this._kuitansi;
+    },
+    set tob(tob) {
+        this._tob = tob;
+    },
+    get tob() {
+        return this._tob;
+    },
+    set modul_id_source(modul_id_source) {
+        this._modul_id_source = modul_id_source;
+    },
+    get modul_id_source() {
+        return this._modul_id_source;
+    },
+    set giro_pokok(giro_pokok) {
+        this._giro_pokok = giro_pokok;
+    },
+    get giro_pokok() {
+        return this._giro_pokok;
+    },
+    set tab_pokok(tab_pokok) {
+        this._tab_pokok = tab_pokok;
+    },
+    get tab_pokok() {
+        return this._tab_pokok;
+    },
+    set dep_pokok(dep_pokok) {
+        this._dep_pokok = dep_pokok;
+    },
+    get dep_pokok() {
+        return this._dep_pokok;
+    },
+    set keterangan(keterangan) {
+        this._keterangan = keterangan;
+    },
+    get keterangan() {
+        return this._keterangan;
+    },
+    set verifikasi(verifikasi) {
+        this._verifikasi = verifikasi;
+    },
+    get verifikasi() {
+        return this._verifikasi;
+    },
+    set userid(userid) {
+        this._userid = userid;
+    },
+    get userid() {
+        return this._userid;
+    },
+    set kode_trans(kode_trans) {
+        this._kode_trans = kode_trans;
+    },
+    get kode_trans() {
+        return this._kode_trans;
+    },
+    set kode_kantor(kode_kantor) {
+        this._kode_kantor = kode_kantor;
+    },
+    get kode_kantor() {
+        return this._kode_kantor;
+    },
+    set jam(jam) {
+        this._jam = jam;
+    },
+    get jam() {
+        return this._jam;
+    },
+    set ip_add(ip_add) {
+        this._ip_add = ip_add;
+    },
+    get ip_add() {
+        return this._ip_add;
+    },
+    set kuitansi_id(kuitansi_id) {
+        this._kuitansi_id = kuitansi_id;
+    },
+    get kuitansi_id() {
+        return this._kuitansi_id;
+    }
+};
+
+module.exports = {
+    abatrans
+};
