f20dd4f849c2ff5bda0caded7d5da72cb0273ed2
wolfz <azhar.zakiy@gmail.com>
2019-11-03 20:02:00 +0700

    fix bugs no balance jurnal

--- controllers/global_controller.js
+++ controllers/global_controller.js
@@ -104,16 +104,17 @@ module.exports = {
             return res.send(utility.GiveResponse("01", "PARAMETER TYPE KOSONG"));
         }
         if (params.type === 'tabungan') {
+            let countNoBalance = await accounting.GetTabJurnalNoBalance(params.trans_id);
             pool.getConnection(function (err, connection) {
                 let sqlString = `SELECT tabtrans_id,kode_kantor,TGL_TRANS,NO_REKENING,POKOK,KETERANGAN 
                 FROM tabtrans WHERE tabtrans_id=? AND kuitansi_id=? AND kode_kantor=?`;
                 connection.query(sqlString, [params.trans_id, params.kuitansi_id, params.kode_kantor], function (err, rows) {
                     if (!err && rows.length > 0) {
-                        let countNoBalance = accounting.GetTabJurnalNoBalance(params.trans_id);
-                        if (countNoBalance > 0) {
-                            responseBody = utility.GiveResponse("00", "TRANSAKSI SUKSES", rows);
-                        } else {
+                        console.log(countNoBalance);
+                        if (countNoBalance === 1) {
                             responseBody = utility.GiveResponse("01", "TRANSAKSI PERLU REPOSTING (JURNAL NO BALANCE)", rows);
+                        } else {
+                            responseBody = utility.GiveResponse("00", "TRANSAKSI SUKSES", rows);
                         }
                     } else {
                         responseBody = utility.GiveResponse("01", "TRANSAKSI TIDAK DITEMUKAN");
--- helpers/accounting.js
+++ helpers/accounting.js
@@ -4,26 +4,25 @@ module.exports = {
         return new Promise(resolve => {
             let returnId = 0;
             pool.getConnection(function (err, connection) {
-                let sqlString = `SELECT tabtrans_id FROM tabtrans WHERE tabtrans_id = ? 
-                AND kuitansi_id NOT IN ( SELECT kuitansi_id FROM transaksi_master WHERE 
-                kuitansi_id IS NOT NULL AND TRANS_ID_SOURCE = '11877801' ) UNION ALL 
-                SELECT tabtrans_id FROM tabtrans WHERE tabtrans_id = ? AND kuitansi_id IN (
-                SELECT kuitansi_id FROM transaksi_master WHERE trans_id IN (
-                SELECT x.MASTER_ID FROM ( SELECT MASTER_ID, sum( debet ) debet,
-                sum( kredit ) kredit FROM transaksi_detail WHERE 
-                master_id IN ( SELECT trans_id FROM transaksi_master WHERE KODE_JURNAL = 'TAB' 
-                AND TRANS_ID_SOURCE = '11877801' ) GROUP BY master_id ) x WHERE
-                debet <> kredit))`;
-                connection.query(sqlString,
-                    [tabtrans_id], function (err, rows) {
-                        if (!err && rows.length > 0) {
-                            returnId = 1;
-                            resolve(returnId);
-                        } else {
-                            returnId = 0;
-                            console.error(`GAGAL GET JURNAL TAB NO BALANCE : ${err.message}`);
-                        }
-                    });
+                let sqlString = 'SELECT tabtrans_id FROM tabtrans WHERE tabtrans_id = ' + tabtrans_id + ' ' +
+                    'AND kuitansi_id NOT IN ( SELECT kuitansi_id FROM transaksi_master WHERE ' +
+                    'kuitansi_id IS NOT NULL AND TRANS_ID_SOURCE = ' + tabtrans_id + ' ) ' +
+                    'UNION ALL SELECT tabtrans_id FROM tabtrans WHERE tabtrans_id = ' + tabtrans_id + ' ' +
+                    'AND kuitansi_id IN ( SELECT kuitansi_id FROM transaksi_master WHERE trans_id IN ' +
+                    '(SELECT x.MASTER_ID FROM (SELECT MASTER_ID,sum( debet ) debet,sum(kredit ) kredit ' +
+                    'FROM transaksi_detail WHERE master_id IN ( SELECT trans_id FROM transaksi_master ' +
+                    'WHERE KODE_JURNAL = "TAB" AND TRANS_ID_SOURCE = ' + tabtrans_id + ' ) GROUP BY master_id ) x ' +
+                    'WHERE debet <> kredit ))';
+                connection.query(sqlString, function (err, rows) {
+                    if (err) {
+                        returnId = 0;
+                    } else if (rows.length === 0) {
+                        returnId = 0;
+                    } else {
+                        returnId = 1;
+                    }
+                    resolve(returnId);
+                });
                 connection.release();
             });
         });
