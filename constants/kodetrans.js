1767fa3aeae7cfdfd0a1a93e85b8e85d1573515a
wolfz <azhar.zakiy@gmail.com>
2019-12-17 22:18:42 +0700

    [const][apicode] add apicode VA COA

--- constants/kodetrans.js
+++ constants/kodetrans.js
@@ -4,6 +4,7 @@ module.exports = Object.freeze({
         kodeTransSetorTunai: '100',
         kodeTransTarikTunai: '200',
         kodeTransSetorCoa: '102',
+        kodeTransSetorVA: '190',
         kodeTransTarikCoa: '202',
         kodeTransTransfer: '204',
         kodeTransSetorABA: '115',
